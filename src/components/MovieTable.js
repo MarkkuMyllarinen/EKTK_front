0c5544598bc678f38d343e73e689fb43788c9bc2
HotSpor <hotspor00@gmail.com>
2020-04-12 16:28:01 +0300

    First commit / fetch from local / added table

---
+++ src/components/MovieTable.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import 'react-table-v6/react-table.css';
+import ReactTable from "react-table-v6";
+
+function MovieTable() {
+
+    const [listItems, setListItems] = React.useState([]);
+
+
+    function fetchData() {
+        fetch('http://localhost:8080/movies')
+            .then(response => response.json())
+            .then(responseData => {
+                setListItems(responseData);
+            })
+    }
+
+    React.useEffect(() => {
+        fetchData();
+    }, []);
+
+
+    const columns = [{
+        Header: 'Movie',
+        accessor: 'movieName'
+    }, {
+        Header: 'Year',
+        accessor: 'year',
+    }, {
+        Header: 'Rating',
+        accessor: 'rating',
+    }];
+
+    const defaultSorted = [{
+        id: "stringTime",
+        desc: true
+    }];
+
+    return (
+        <div className="App">
+            <ReactTable data={listItems}
+                        columns={columns} sortable={true}
+                        defaultPageSize={20}
+                        defaultSorted={defaultSorted}/>
+        </div>
+    );
+}
+
+export default MovieTable;
